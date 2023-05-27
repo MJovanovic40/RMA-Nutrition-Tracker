2047e81e69c02a8e5db082e1a4a367cb02207c63
Milan Jovanović <milanj1705@gmail.com>
2023-05-27 17:18:22 +0200

    Initial commit

---
+++ app/src/main/java/rs/raf/projekat_jun_nikola_gavrilovic_rn7822_milan_jovanovic_rn4020/MainActivity.java
@@ -0,0 +1,14 @@
+package rs.raf.projekat_jun_nikola_gavrilovic_rn7822_milan_jovanovic_rn4020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
