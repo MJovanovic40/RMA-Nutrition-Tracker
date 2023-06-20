4613e343288c3f6a77a5ba817e5408e40c242886
Milan Jovanović <milanj1705@gmail.com>
2023-06-20 18:54:53 +0200

    Disable action bar

--- app/src/main/java/rs/raf/projekat_jun_nikola_gavrilovic_rn7822_milan_jovanovic_rn4020/AppState.java
+++ app/src/main/java/rs/raf/projekat_jun_nikola_gavrilovic_rn7822_milan_jovanovic_rn4020/AppState.java
@@ -2,7 +2,7 @@ package rs.raf.projekat_jun_nikola_gavrilovic_rn7822_milan_jovanovic_rn4020;
 
 public class AppState {
     private static AppState instance = null;
-    
+
     public static AppState getInstance() {
         if(instance == null){
             instance = new AppState();
